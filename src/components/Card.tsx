79e8a6caf3d9aee39e7d1a67e75e222b2d6fa065
DLineone <DLineone@users.noreply.github.com>
2024-03-19 01:07:04 +0300

    main

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,9 +11,9 @@ const bnbAdress = "0xB8c77482e45F1F44dE1745F52C74426C631bDD52";
 
 export default function Card() {
     const { sdk, connected, connecting } = useSDK();
-    const [account, setAccount] = useState(undefined);
-    const [bnbBalance, setBnbBalance] = useState(undefined);
-    const [ethBalance, setEthBalance] = useState(undefined);
+    const [account, setAccount] = useState<stgin | null>(null);
+    const [bnbBalance, setBnbBalance] = useState<number | null>(null);
+    const [ethBalance, setEthBalance] = useState<number | null>(null);
 
 
     useEffect(() => {
